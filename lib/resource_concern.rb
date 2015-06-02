05fcb2ae4598674371ade3debb63b6717ed023dd
Jason Olim <jason@olim.org>
2015-06-01 21:01:58 -0400

    Update resource_concern.rb

--- lib/resource_concern.rb
+++ lib/resource_concern.rb
@@ -5,8 +5,6 @@ module ResourceConcern
     before_action :get_resource, only: [:destroy, :edit, :show, :update]
   end
 
-private 
-
   def get_resource
     set_resource_name
     if !@resource_class.constantize.exists?(params[:id])
@@ -16,6 +14,8 @@ private
     eval "@#{@resource_name} = #{@resource_class}.find params[:id]"
     eval "@resource = @#{@resource_name}"
   end
+  
+private 
 
   def set_resource_name
     @resource_class = self.class.to_s.gsub(/Controller/,'').gsub(/.*::/,'').singularize
